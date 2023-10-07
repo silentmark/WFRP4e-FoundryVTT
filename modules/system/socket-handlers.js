bd95858790b22450a7f4d9ece40134855c19cdc8
Marek Kembrowski <marek.kembrowski@se.gt.com>
2023-10-07 22:47:16 +0200

    fix for test requests over sockets

--- modules/system/socket-handlers.js
+++ modules/system/socket-handlers.js
@@ -152,11 +152,15 @@ export default class SocketHandlers  {
                 let trait = data.payload.trait;
                 test = await actor.setupTrait(trait, options);
             }
-            if (owner.isGM) {
+            if (owner.isGM && test) {
                 await test.roll();
             }
-            let message = game.messages.get(messageId);        
-            await message.update({"flags.data.test": test});
+            let message = game.messages.get(messageId);
+            if (test) {
+                await message.update({"flags.data.test": test});
+            } else {
+                await message.delete();
+            }
         }
     }
 
